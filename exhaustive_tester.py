3ccee2a5483819c1f3f5d0c3bda95b8382e99c75
Sichoel Sung <sicheol.sung@yonsei.ac.kr>
2023-07-19 18:02:07 +0900

    feat: Compare `test_case_generater` and CCFG

--- exhaustive_tester.py
+++ exhaustive_tester.py
@@ -1,43 +1,75 @@
 import jsonlines
 import traceback
 import sys
-import logging
 
 
 from tqdm import tqdm
 from counting_context_free_grammar import CountingContextFreeGrammar as CCFG
 
-from discriminator import Discriminator
-from generator import TestCaseGenerator
+from discriminator import discriminator as Discriminator
+from generator import test_case_generator as TestCaseGenerator
+
+DEFAULT_MAX_ITER = 100
 
 
-DEFAULT_MAX_ITER = 1
-
-
-def test_parser(parser, grammar, constraints, test_cases):
+def test_parser(
+    parser: Discriminator,
+    grammar: list[str],
+    constraints: list[str],
+    test_cases: list[str]
+):
     for test_case in test_cases:
         parsed_result = parser(grammar, constraints, test_case)
         if not parsed_result:
             raise Exception("Wrong parsing result")
 
 
-def test_generator(generator, parser, grammar, constraints, k=DEFAULT_MAX_ITER):
+def test_generator(
+    generator: TestCaseGenerator,
+    parser: Discriminator,
+    grammar: list[str],
+    constraints: list[str],
+    k: int = DEFAULT_MAX_ITER
+):
     test_cases = [generator(grammar, constraints) for _ in range(k)]
     for test_case in test_cases:
-        parsed_result = parser(grammar, constraints, test_case)
-        if not parsed_result:
+        parsed_result = None
+        try:
+            parsed_result = parser(grammar, constraints, test_case)
+        except Exception:
+            pass
+
+        if parsed_result is None:
             raise Exception(f"Wrong generation result\n{test_case}")
     return test_cases
 
 
-def test_ccfg(ccfg, parser, k=DEFAULT_MAX_ITER):
-    for _ in range(k):
-        test_case = ccfg.generate()
+def test_ccfg(
+    ccfg: CCFG,
+    parser: Discriminator,
+    k: int = DEFAULT_MAX_ITER
+):
+    test_cases = [ccfg.generate() for _ in range(k)]
+    for test_case in test_cases:
+        parsed_result = None
         try:
-            parser(grammar, constraints, test_case)
-        except Exception as e:
-            print(test_case)
-            raise e
+            parsed_result = parser(grammar, constraints, test_case)
+        except Exception:
+            pass
+
+        if parsed_result is None:
+            raise Exception(f"Wrong generation result\n{test_case}")
+
+
+def print_error(name: str, e: Exception):
+    print()
+    print("#" * 50)
+    print(f'{name} exception on idx: {problem_idx}')
+    print("Error:", e)
+    for k, v in specification.items():
+        print(k, v)
+    print("#" * 50)
+    traceback.print_exc(file=sys.stdout)
 
 
 if __name__ == "__main__":
@@ -73,32 +105,30 @@ if __name__ == "__main__":
             try:
                 test_cases = test_generator(
                     generator, parser, grammar, constraints)
-            except Exception:
+            except Exception as e:
                 generator_errors.append(problem_idx)
-                continue
+                print_error("generator", e)
 
             ccfg = None
             try:
-                ccfg = CCFG(grammar, constraints)
+                ccfg = CCFG(grammar, constraints, testmode=True)
                 test_ccfg(ccfg, parser, k=DEFAULT_MAX_ITER)
             except Exception as e:
                 ccfg_errors.append(problem_idx)
-                print()
-                print("#" * 50)
-                print(f'CCFG exception on idx: {problem_idx}')
-                print("Error:", e)
-                for k, v in specification.items():
-                    print(k, v)
-                print(ccfg)
-                print("#" * 50)
-                traceback.print_exc(file=sys.stdout)
+                print_error("CCFG", e)
 
     parser_passed = total - len(parser_errors)
-    generator_passed = total - len(generator_errors)
-    ccfg_passed = total - len(ccfg_errors)
+    generator_passed = total - len(generator_errors) - len(parser_errors)
+    ccfg_passed = total - len(ccfg_errors) - len(parser_errors)
 
     print(f"Parser: {parser_passed}/{total}")
     print(f"Generator: {generator_passed}/{total}")
     print(f"CCFG: {ccfg_passed}/{total}")
 
-    print(ccfg_errors)
+    generator_only_error = sorted(set(generator_errors) - set(ccfg_errors))
+    ccfg_only_error = sorted(set(ccfg_errors) - set(generator_errors))
+    both_error = sorted(set(generator_errors) & set(ccfg_errors))
+
+    print(f"Generator only error {generator_only_error}")
+    print(f"CCFG only error {ccfg_only_error}")
+    print(f"Both error {both_error}")

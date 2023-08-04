f8e506f112732a2e28d19c6141581c985134ac15
Oraison <dodo1399@naver.com>
2023-08-04 16:07:05 +0900

    [Update] add comments

--- test_result.bash
+++ test_result.bash
@@ -6,6 +6,13 @@ if [ $# -ge 2 ]; then
 else
     declare -i num_variable=20
 fi
+
+# $1 : 1번째 파라미터
+#      저장된 모델 이름
+#
+# $2 : 테스트할 epoch 범위(옵션)
+#      0부터 $2까지의 epoch의 test set inference 결과물을 확인
+
 # for i in {0..$num_variable..1}
 for ((i = 0; i < $num_variable; i++))
     do 

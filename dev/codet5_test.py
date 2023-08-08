d539eb6d083e36316323e11a023acbab5048733b
Sichoel Sung <sicheol.sung@yonsei.ac.kr>
2023-08-08 17:28:24 +0900

    refactor: Move dev-only scripts to dev

---
+++ dev/codet5_test.py
@@ -0,0 +1,20 @@
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
+import torch
+
+checkpoint = "Salesforce/codet5p-2b"
+device = "cuda"  # for GPU usage or "cpu" for CPU usage
+
+tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+model = AutoModelForSeq2SeqLM.from_pretrained(
+    checkpoint,
+    torch_dtype=torch.float16,
+    low_cpu_mem_usage=True,
+    trust_remote_code=True
+).to(device)
+
+encoding = tokenizer(
+    "def print_hello_world():", return_tensors="pt").to(device)
+encoding['decoder_input_ids'] = encoding['input_ids'].clone()
+outputs = model.generate(**encoding, max_length=15)
+print(tokenizer.decode(outputs[0], skip_special_tokens=True))
+

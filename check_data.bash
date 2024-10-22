f8d531ec94c8025e077ba893873a6693b4856697
Sicheol Sung <cdltlehf@gmail.com>
2024-10-22 10:25:02 +0200

    test: simplify list

--- check_data.bash
+++ check_data.bash
@@ -1,11 +1,11 @@
 shopt -s globstar
 
 for f in ${DATA_DIR}/{execution-summary,generation-result,grammar,parsing-result}/**/test.jsonl ${RAW_DATA_DIR}/{grammar,level,testcase}/**/test.jsonl; do
-  echo -n $f
   filename=$(basename -- "$f" ".jsonl")
   if jq '.["name"]' -r $f | diff - "raw-data/problem-list/${filename}.txt" > /dev/null 2>&1; then
-    echo " - OK"
+    true
   else
+    echo -n $f
     echo " - Failed"
   fi
 done;
